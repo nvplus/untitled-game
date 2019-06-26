06cda9bc0f8ec9a17cdab1acf41ff75c8bc3bf32
Vincent <v.ngu@outlook.com>
2019-06-26 01:20:20 -0400

    completed movement and collision detection

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ grid = [
     [0, 0, 0, 0],
     [0, 2, 2, 0],
     [0, 2, 2, 0],
-    [0, 1, 0, 0]
+    [0, 0, 1, 0]
 ]
 
 def read_position(grid):
@@ -16,38 +16,40 @@ def read_position(grid):
     return [-1, -1]
 
 def check_collision(position):
-    return False
+    """
+    Returns True if player has collided with a wall or has gone out of bounds
+    """
+    try:
+        grid[position[1] - 1][position[0] - 1]
+    except:
+        print("DEBUG: Out of bounds detected.")
+        return True
+
+    print("DEBUG: Collision detected.")
+    return grid[position[1] - 1][position[0] - 1] is not 0
 
 def update_board(old_pos, new_pos):
-    grid[old_pos[1] - 1][old_pos[1] - 1] = 0 
-    grid[new_pos[1] - 1][new_pos[1] - 1] = 1 
+    grid[old_pos[1] - 1][old_pos[0] - 1] = 0
+    grid[new_pos[1] - 1][new_pos[0] - 1] = 1 
 
 def do_movement(player_input, grid):
     pos = read_position(grid)
     new_pos = pos.copy()
 
-    if player_input is "x":
-        raise Exception("Done")
-
-    elif player_input is "l":
+    if player_input is "l":
         new_pos[0] -= 1
-        print(new_pos)
 
     elif player_input is "d":
-        new_pos[0][0] += 1
+        new_pos = [new_pos[0], new_pos[1] + 1]
 
     elif player_input is "u":
-        new_pos[0] -= 1
+        new_pos = [new_pos[0], new_pos[1] - 1]
 
     elif player_input is "r":
-        new_pos[0][0] += 1
-
-    print("Move: " + str(pos) + " -> " + str(new_pos))
+        new_pos[0] += 1
 
     if not check_collision(new_pos):
         update_board(pos, new_pos)
-    
-    return
 
 def convert_to_game_char(value):
     if value is 0:
@@ -73,4 +75,11 @@ while not done:
 
     player_input = input("Enter an input: ")
 
-    do_movement(player_input, grid)
+    if player_input == "exit":
+        print("\nThank you for playing!")
+        done = True
+
+    else:
+        do_movement(player_input, grid)
+
+        print()
